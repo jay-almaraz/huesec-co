0dc7aa9b3926505cee6bbc703a5c0f17ca76cb3a
jay-almaraz <jay.amgrant@gmail.com>
2021-05-03 13:14:06 +1000

    remove need for custom image

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-docker run -it --rm -p 3000:3000 -v "$PWD":/usr/src/app -w /usr/src/app --name huesec-co huesec-co-node-app yarn dev 
+docker run -it --rm -p 3000:3000 -v "$PWD":/usr/src/app -w /usr/src/app --name huesec-co node:14 yarn dev 
